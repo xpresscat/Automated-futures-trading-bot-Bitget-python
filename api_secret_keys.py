e8e243737908f023046625201a4494d8e9089366
xpresscat <xpresscat@hotmail.com>
2025-06-02 18:19:29 +0100

    Add files via upload

---
+++ api_secret_keys.py
@@ -0,0 +1,11 @@
+#Donations in crypto on all networks (evm): 0xeFbD1589309C0407c884283b776704272554ac82
+
+apiKey = "your bitget apiKey"
+secretKey = "your bitget secretKey"
+passphrase = "your bitget passphrase"
+
+
+
+
+
+
